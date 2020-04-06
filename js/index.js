33a94af03f3473b88bc450a18b595485b0a44124
preyansh98 <preyansh98@gmail.com>
2020-04-06 05:29:44 -0400

    add styling to contents page

--- js/index.js
+++ js/index.js
@@ -15,6 +15,9 @@ const darkenElementsByClassName = (classesToDarken) => {
 const darken = () => {
     let classesToDarken = []; 
 
+    //Body
+    classesToDarken.push('d2l-body');
+
     //Front Page -> Navigation: 
     classesToDarken.push('d2l-branding-navigation-dark-foreground-color');
     classesToDarken.push('d2l-branding-navigation-background-color');
@@ -22,6 +25,14 @@ const darken = () => {
 
     //Front Page -> Tiles:
     classesToDarken.push('d2l-tiles-container');
+
+    //Content Page -> sidebar:
+    classesToDarken.push('d2l-twopanelselector-side-padding');
+    classesToDarken.push('d2l-twopanelselector-padding');
+
+    let searchBarBg = document.getElementsByClassName('d2l-twopanelselector-side');
+    if(searchBarBg.length > 0)
+        searchBarBg[0].style.background = 'black';
     
     //Darken listed classes. 
     darkenElementsByClassName(classesToDarken);
@@ -36,6 +47,9 @@ const darken = () => {
 /* BEGIN INVERT */
 
     //Color all Nav Links White. 
+    if(document.getElementsByClassName('d2l-navigation-s-group-text')[0])
+        document.getElementsByClassName('d2l-navigation-s-group-text')[0].style.color = "white"
+    
     let navLinks =  document.getElementsByClassName('d2l-navigation-s-link');
     for(let i = 0; i<navLinks.length; ++i)
         navLinks[i].style.color = _invert;
@@ -45,6 +59,11 @@ const darken = () => {
     for(let i = 0; i<texts.length; ++i)
         texts[i].style.color = _invert;
     
+    //Color all headings white.
+    let headings = document.getElementsByClassName('d2l-heading vui-heading-4')
+    for(let i = 0; i<headings.length; ++i)
+        headings[i].style.color = _invert; 
+
 
     //Course Text
 } 
